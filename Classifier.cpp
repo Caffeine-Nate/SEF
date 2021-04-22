d3fa5a078694ac4c9156c90841cdea579f15bdaa
Nathan <ndom13@gmail.com>
2021-04-21 20:40:59 -0400

    Created Classifier class

---
+++ Classifier.cpp
@@ -0,0 +1,13 @@
+/*
+* FILE : Classifier.cpp
+* PROJECT : PROG1350 - Assignment 5
+* PROGRAMMER : Nathan Domingo
+* FIRST VERSION : 2021-04-21
+* DESCRIPTION :  Functions for Assignment 5
+*/
+#include<stdio.h>
+
+class Classifier
+{
+	int data;
+};
